2c280e3ecd56673cdcf84d64940d0ccc2f53df30
Damon <damon@damongolding.com>
2022-10-11 16:30:29 +0100

    Adding Vite for dev server and build

---
+++ vite.config.js
@@ -0,0 +1,44 @@
+/** @type {import('vite').UserConfig} */
+import { defineConfig } from "vite";
+
+import fg from "fast-glob";
+import { parse, resolve } from "path";
+
+const pagesGlob = await fg("pages/**/*.html", {
+    dot: false,
+});
+
+const pages = pagesGlob.reduce((acc, current) => {
+    const pageParse = parse(current);
+    const pagePath = pageParse.dir.split("/");
+    const pageName = pagePath.pop();
+    const page = {
+        [pageParse.name]: resolve(
+            __dirname,
+            ...pagePath,
+            pageName,
+            `${pageParse.name}${pageParse.ext}`
+        ),
+    };
+    return { ...acc, ...page };
+}, {});
+
+export default defineConfig(({ command, mode }) => {
+    process.env = {
+        ...process.env,
+        VITE_pages: pages,
+    };
+
+    return {
+        root: "",
+        server: {
+            open: "/pages/index.html",
+        },
+        build: {
+            rollupOptions: {
+                input: pages,
+            },
+            emptyOutDir: true,
+        },
+    };
+});
